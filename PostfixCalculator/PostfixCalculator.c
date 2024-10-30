43ea38e4a9660631f2d0917f20ed51f6a7656ee8
Alena-pa <alena.pakhnusheva@gmail.com>
2024-10-30 10:57:48 +0300

    Add PostfixCalculator

--- PostfixCalculator/PostfixCalculator.c
+++ PostfixCalculator/PostfixCalculator.c
@@ -1,4 +1,23 @@
-int postfixCalculator(char *string)
-{
+#include <stdio.h>
+#include <stdbool.h>
 
+int postfixCalculator(char *string){
+    
+}
+
+
+bool test(){
+    char firstString[5] = {"1", "2", "+", "3", "-"};
+    char secondString[5] = {"+", "1", "2", "-", "3"};
+    char thirdString[5] = {"1", "2", "+", "3", "4"};
+    if (postfixCalculator(firstString) == 0 && postfixCalculator(secondString) == 1 && postfixCalculator(thirdString) == 1){
+        return true;
+    }
+    return false;
+}
+
+int main(){
+    if (!test){
+        printf("Test Failed!");
+    }
 }
