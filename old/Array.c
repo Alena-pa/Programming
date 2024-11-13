37a482a562773d7081213a1f92eaf2342ff19c50
Alena-pa <alena.pakhnusheva@gmail.com>
2024-11-13 23:03:59 +0300

    Fix array

--- old/Array.c
+++ old/Array.c
@@ -12,9 +12,8 @@ void swap(int *left, int *right)
 
 void reverseSubArrayOfIntegers(int* arrayOfIntegers, int begin, int end)
 {
-    for (;begin < end; begin++, end--)
-    {
-        swap(arrayOfIntegers + begin, arrayOfIntegers + end);
+    for (;begin < end; begin++, end--){
+        swap(&arrayOfIntegers[begin], &arrayOfIntegers[end]);
     }
 }
 
@@ -25,12 +24,17 @@ void printArray(int *arrayOfIntegers, int lenghtOfArray)
     printf("\n");
 }
 
-int main()
+void reverseSegments(int *arrayOfIntegers, int lenghtOfArray){
+    int middle = lenghtOfArray / 2;
+    reverseSubArrayOfIntegers(arrayOfIntegers, 0, middle - 1);
+    reverseSubArrayOfIntegers(arrayOfIntegers, middle, lenghtOfArray - 1);
+    reverseSubArrayOfIntegers(arrayOfIntegers, 0, lenghtOfArray - 1);
+}
+
+int main(void)
 {
     int arrayOfIntegers[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     printArray(arrayOfIntegers, 9);
-    reverseSubArrayOfIntegers(arrayOfIntegers, 0, 4);
-    reverseSubArrayOfIntegers(arrayOfIntegers, 5, 8);
-    reverseSubArrayOfIntegers(arrayOfIntegers, 0, 8);
+    reverseSegments(arrayOfIntegers, 9);
     printArray(arrayOfIntegers, 9);
 }
