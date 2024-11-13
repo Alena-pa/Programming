9135230df832dd413adc6fe305ac223e5645e9e5
Alena-pa <alena.pakhnusheva@gmail.com>
2024-11-13 23:06:02 +0300

    Update and rename Task_8.c to Array.c

---
+++ old/Array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void swap(int *left, int *right){
+    if (left != right){
+        *left ^= *right;
+        *right ^= *left;
+        *left ^= *right;
+    }
+}
+
+void reverseSubArrayOfIntegers(int* arrayOfIntegers, int begin, int end){
+    for (;begin < end; begin++, end--){
+        swap(&arrayOfIntegers[begin], &arrayOfIntegers[end]);
+    }
+}
+
+void printArray(int *arrayOfIntegers, int lenghtOfArray){
+    for (int i = 0; i < lenghtOfArray; i++){
+        printf("%d\n", arrayOfIntegers[i]);
+    }
+}
+
+void reverseSegments(int *arrayOfIntegers, int lenghtOfArray){
+    int middle = lenghtOfArray / 2;
+    reverseSubArrayOfIntegers(arrayOfIntegers, 0, middle - 1);
+    reverseSubArrayOfIntegers(arrayOfIntegers, middle, lenghtOfArray - 1);
+    reverseSubArrayOfIntegers(arrayOfIntegers, 0, lenghtOfArray - 1);
+}
+
+int main(void){
+    int arrayOfIntegers[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    printArray(arrayOfIntegers, 9);
+    reverseSegments(arrayOfIntegers, 9);
+    printArray(arrayOfIntegers, 9);
+}
