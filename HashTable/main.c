2d6ff2fffe1037352d02f5283a0a81600096ed62
Alena-pa <alena.pakhnusheva@gmail.com>
2024-12-03 20:02:23 +0300

    Write main.c

--- HashTable/main.c
+++ HashTable/main.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+#include "HashTable.h"
+#include "Tests.h"
+
+int main(void) {
+    HashTable* table = createTable(100);
+    analyzeFile(table, "file.txt");
+    printTable(table);
+    analyzeTable(table);
+
+    return 0;
+}
