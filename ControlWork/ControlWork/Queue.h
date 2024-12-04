1b1a28febd42994c719667e72adae7a1a1f14fca
Alena-pa <alena.pakhnusheva@gmail.com>
2024-12-04 15:20:15 +0300

    add comments

--- ControlWork/ControlWork/Queue.h
+++ ControlWork/ControlWork/Queue.h
@@ -10,14 +10,20 @@ typedef struct Queue {
     int length;
 } Queue;
 
+// Function to check if the queue full
 bool isFull(struct Queue* queue);
 
+// Function to create queue
 void createQueue(struct Queue* queue);
 
+// Function to loop the queue
 void incrementQueue(struct Queue* queue);
 
+// Function to remove an element from the queue
 int dequeue(struct Queue* queue);
 
+// Function to add an element to the queue 
 void enqueue(struct Queue* queue, int value);
 
+// Function to print current queue
 void printQueue(struct Queue* queue);
