26c171277044276d6f6aab65e11cf3c5a41668fd
Alena-pa <alena.pakhnusheva@gmail.com>
2024-12-18 15:16:58 +0300

    Try to add queue

--- ThirdControlWork/Queue.h
+++ ThirdControlWork/Queue.h
@@ -1,10 +1,21 @@
 #pragma once
 
-typedef struct firstStack firstStack;
-
-typedef struct secondStack secondStack;
+typedef struct Stack Stack;
 
 typedef struct Queue Queue;
 
+Stack* createStack();
+
 void createQueue(struct Queue* queue);
 
+bool isFull(struct Queue* queue);
+
+int dequeue(struct secondStack* stack);
+
+int enqueue(struct firstStack* stack, int value);
+
+void push(struct firstStack* stack, int value);
+
+void pop(struct secondStack* stack);
+
+void deleteQueue(struct Queue* queue);
