64bd68d00ad4d292d27e8b5a898b6060d6356bd6
Alena-pa <alena.pakhnusheva@gmail.com>
2025-01-09 15:46:31 +0300

    Add commeents

--- SortingTree/Tests.h
+++ SortingTree/Tests.h
@@ -1,16 +1,23 @@
 #pragma once
 #include <stdbool.h>
 
+//Checking if an element was added correctly
 bool testAddElement();
 
+//Checking the correctness of the element location
 bool testFindNodeByKey();
 
+//Checking for correctness of element existence check
 bool testExistenceOfElement();
 
+//Checking if an element without children was deleted correctly
 bool testDeleteElementWithoutChildrens();
 
+//Checking if deleting an element with one child is correct
 bool testDeleteElementWithOneChildren();
 
+//Checking if deleting an element with multiple children is correct
 bool testDeleteElementWithManyChildrens();
 
+//Combining tests
 bool allTests();
