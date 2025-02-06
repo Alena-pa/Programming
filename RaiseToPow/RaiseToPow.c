ae75aa2753c38246cfa59f15565c51fa1b467a66
Alena-pa <alena.pakhnusheva@gmail.com>
2025-02-06 15:13:22 +0300

    Added RaiseToPow using comments

--- RaiseToPow/RaiseToPow.c
+++ RaiseToPow/RaiseToPow.c
@@ -8,12 +8,7 @@ double simpleRaiseToPow(int number, int pow) {
     for (int i = 0; i < absPow; i++) {
         answer *= number;
     }
-    if (pow < 0) {
-        return 1 / answer;
-    }
-    else {
-        return answer;
-    }
+    return (pow < 0) ? (1 / answer) : answer;
 }
 
 double upgradeRaiseToPow(int number, int pow) {
@@ -40,14 +35,15 @@ double upgradeRaiseToPow(int number, int pow) {
 bool areEqual(double firstNumber, double secondNumber, double epsilon) {
     return fabs(firstNumber - secondNumber) < epsilon;
 }
-bool testSimpleRaise() {
+
+bool testSimpleRaise(void) {
     double epsilon = 1e-6;
     bool result = areEqual(simpleRaiseToPow(2, 2), 4, epsilon) && areEqual(simpleRaiseToPow(2, 0), 1, epsilon);
     bool result1 = areEqual(simpleRaiseToPow(-2, 1), -2, epsilon) && areEqual(simpleRaiseToPow(2, -1), 0.5, epsilon) && areEqual(simpleRaiseToPow(-2, 2), 4, epsilon);
     return result && result1;
 }
 
-bool testUpgradeRaise() {
+bool testUpgradeRaise(void) {
     double epsilon = 1e-6;
     bool result = areEqual(upgradeRaiseToPow(2, 2), 4, epsilon) && areEqual(upgradeRaiseToPow(2, 0), 1, epsilon);
     bool result1 = areEqual(upgradeRaiseToPow(-2, 2), 4, epsilon) && areEqual(upgradeRaiseToPow(2, -1), 0.5, epsilon);
