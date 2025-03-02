503f5dcf7b1cd44812cae7009a22b93dac1b0828
Alena-pa <alena.pakhnusheva@gmail.com>
2025-03-02 22:36:50 +0300

    fix names

--- SyntaxTree/SyntaxTree.h
+++ SyntaxTree/SyntaxTree.h
@@ -2,21 +2,14 @@
 #include <stdio.h>
 
 typedef struct Node Node;
-
-// Get left child
-Node* getLeftChild(Node* node);
-
-// Get right child
-Node* getRightChild(Node* node);
-
 // Remove tree
 void freeTree(Node* node);
 
 // Fill a parse tree with an arithmetic expression
-Node* splitArithmeticExpression(FILE *file);
+Node* parseFile(char* nameOfFile);
 
 // Print tree
 void printTree(Node* node);
 
 // Calculate the value of an expression by traversing a tree
-int calculation(Node* node, int* errorCode);
+int calculate(Node* node, int* errorCode);
