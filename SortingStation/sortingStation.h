114644495264956e704034aa80f98aa1481af8eb
Alena-pa <alena.pakhnusheva@gmail.com>
2025-03-03 22:25:52 +0300

    delete useful files from sortingStation.h

--- SortingStation/sortingStation.h
+++ SortingStation/sortingStation.h
@@ -1,13 +1,4 @@
 #pragma once
 
-// checks if element operation or not
-bool isOperation(char element);
-
-// checks if element is digit or not
-bool isDigit(char element);
-
-// defines operator priority
-int precedence(char element);
-
 // convert string to postfix notation
 void convertStringFromInfixToPostfixNotation(const char* stringToConvert, char* outputString, int* errorCode);
